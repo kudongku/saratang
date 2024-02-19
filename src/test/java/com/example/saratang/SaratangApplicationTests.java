28d84b83d4948a439e2d1958d8331a109bbb580f
kudongku <kudongku@naver.com>
2024-02-19 22:39:01 +0900

    chore: init commit

---
+++ src/test/java/com/example/saratang/SaratangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saratang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaratangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
