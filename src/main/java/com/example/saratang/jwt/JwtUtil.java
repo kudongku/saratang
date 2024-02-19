746c81cd1bfd850e2e680799d8122a57e50f3f89
kudongku <kudongku@naver.com>
2024-02-19 23:07:31 +0900

    feat: jwtUtil 생성

---
+++ src/main/java/com/example/saratang/jwt/JwtUtil.java
@@ -0,0 +1,77 @@
+package com.example.saratang.jwt;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+public class JwtUtil {
+
+    private Key key;
+    public final String BEARER_PREFIX = "Bearer";
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    public final String AUTHORIZATION_HEADER = "Authorization";
+
+    @PostConstruct
+    public void init() {
+        String secretKey = "7Iqk7YyM66W07YOA7L2U65Sp7YG065+9U3ByaW5n6rCV7J2Y7Yqc7YSw7LWc7JuQ67mI7J6F64uI64ukLg==";
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public void addToken(String username, HttpServletResponse res) {
+        Date date = new Date();
+
+        String tokenStr = (BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(username) // 사용자 식별자값(ID)
+                        .setExpiration(new Date(date.getTime() + 30 * 60 * 1000L)) // 만료 시간
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact());
+
+        res.setHeader(AUTHORIZATION_HEADER, tokenStr);
+    }
+
+    public String substringToken(String tokenValue) {
+        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
+            return tokenValue.substring(6); // bearer 을 자르고 시작
+        }
+        log.error("Not Found Token");
+        throw new NullPointerException("Not Found Token");
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String getTokenFromRequest(HttpServletRequest req) {
+        return req.getHeader(AUTHORIZATION_HEADER);
+    }
+}
