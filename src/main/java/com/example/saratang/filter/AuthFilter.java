d1a145bce8164f87fba757fe2eecabc72a5ab16f
kudongku <kudongku@naver.com>
2024-02-19 23:06:34 +0900

    feat: filter 생성

---
+++ src/main/java/com/example/saratang/filter/AuthFilter.java
@@ -0,0 +1,61 @@
+package com.example.saratang.filter;
+
+import com.example.saratang.entity.User;
+import com.example.saratang.jwt.JwtUtil;
+import com.example.saratang.repository.UserRepository;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+@Slf4j(topic = "AuthFilter")
+@Component
+@RequiredArgsConstructor
+public class AuthFilter implements Filter {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String url = httpServletRequest.getRequestURI();
+
+        if (StringUtils.hasText(url) &&
+                (url.startsWith("/signup") || url.startsWith("/css") || url.startsWith("/js") || url.startsWith("/login") || url.startsWith("/verifyMail"))
+        ) {
+            log.info("인증처리가 필요없는 URL");
+            chain.doFilter(request, response); // 다음 Filter 로 이동
+        } else {
+            String tokenValue = jwtUtil.getTokenFromRequest(httpServletRequest);
+
+            if (StringUtils.hasText(tokenValue)) { // 토큰이 존재하면 검증 시작
+                // JWT 토큰 substring
+                String token = jwtUtil.substringToken(tokenValue);
+
+                // 토큰 검증
+                if (!jwtUtil.validateToken(token)) {
+                    throw new IllegalArgumentException("Token Error");
+                }
+
+                // 토큰에서 사용자 정보 가져오기
+                Claims info = jwtUtil.getUserInfoFromToken(token);
+
+                User user = userRepository.findByUsername(info.getSubject()).orElseThrow(() ->
+                        new NullPointerException("Not Found User")
+                );
+
+                request.setAttribute("user", user);
+                chain.doFilter(request, response); // 다음 Filter 로 이동
+            } else {
+                throw new IllegalArgumentException("Not Found Token");
+            }
+        }
+    }
+
+}
